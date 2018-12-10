f0ba0da4b1594a50d23a7726b7b1d515cea3f214
yuexxx <ming3ming4@126.com>
2018-12-10 13:48:47 +0800

    添加使用例子

--- src/test/java/com/edward1iao/proxy/ProxyTest.java
+++ src/test/java/com/edward1iao/proxy/ProxyTest.java
@@ -2,16 +2,25 @@ package com.edward1iao.proxy;
 
 import org.junit.Test;
 
-import com.edward1iao.proxy.interfaces.ITest;
+import com.edward1iao.proxy.annotations.HttpProxy;
+import com.edward1iao.proxy.enums.EnumHttpRequestMethod;
+import com.edward1iao.proxy.exception.HttpProxyException;
 import com.edward1iao.proxy.model.TestModel;
-import com.edward1iao.proxy.proxy.HttpProxyHandler;
 
 public class ProxyTest 
 {
     @Test
     public void test()
     {
-    	System.out.println(ProxyFactory.getInstance(ITest.class, HttpProxyHandler.class).getTest0());
-		System.out.println(ProxyFactory.ITEST.getTest4(new TestModel("adsf","asdfddd")));
+    	System.out.println(ProxyFactory.getHttpInstance(IMoni.class).httpPostProxy(new TestModel("ssdfadsf", null)));
+    	System.out.println(ProxyFactory.getHttpInstance(IBaidu.class).doGet());
+    }
+    static interface IBaidu{
+    	@HttpProxy(URL="https://www.baidu.com/")
+    	String doGet();
+    }
+    static interface IMoni{
+    	@HttpProxy(URL="http://172.29.224.89/framework/httpPostProxy/",requestMethod=EnumHttpRequestMethod.POST)
+    	TestModel httpPostProxy(TestModel testModel)throws HttpProxyException;
     }
 }
